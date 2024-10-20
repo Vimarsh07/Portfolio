bbc90e5f66e8b70fcb1cc3bc0674c796de3badad
Vimarsh07 <vimarshpatel2607@gmail.com>
2024-10-20 19:55:00 -0400

    project changes

--- Components/data/ProjectData.js
+++ Components/data/ProjectData.js
@@ -2,7 +2,38 @@
 
 export const Data = {
 
-   "Data-Driven Sales Forecasting and Inventory Management System":{
+  "NLP-Based Sentiment Analysis Platform ":
+  {
+    description: `
+    Develop a comprehensive sentiment analysis platform to process and analyze large datasets of textual content, enabling accurate sentiment classification.
+    Employed Python and libraries such as NLTK, TensorFlow, and Keras to develop the platform. Applied NLP techniques like tokenization, stemming, and lemmatization to preprocess text data and implemented machine learning models for sentiment analysis. `,
+    githubLink: "https://github.com/Vimarsh07/-Sentiment-Classification-using-IMDB-Data",
+  },
+"Stock Analysis Transformer": {
+  description: `
+  Developed a stock prediction model using Transformer architecture with TensorFlow, extracting market sentiment from financial news to achieve a 20% improvement in prediction accuracy.
+  Examined historical stock prices, trading volumes, and economic indicators over the past decade; identified key patterns that enhanced the prediction model's accuracy by 20%, improving forecast reliability
+  Implemented a Trading Environment with PPO and RL (Reinforcement learning) based model.
+  Incorporated technical indicators such as Relative Strength Index (RSI) and Moving Average Convergence Divergence (MACD) to refine predictions, significantly enhancing the accuracy of buy, sell, or hold recommendations.
+  Developed and deployed the model using robust machine learning pipelines, ensuring real-time data processing and timely predictions for effective trading strategies.
+  Leveraged generative AI solutions to simulate market scenarios and generate synthetic financial data, enhancing the model's training process and improving its robustness and predictive capabilities.`,
+  githubLink: "https://github.com/Vimarsh07/Stock-Analysis-Transformer",
+},
+"MindSutra (Mental Health Chatbot)": {
+  description: `A chatbot for empathetic mental health support using fine-tuned Llama models, prompt engineering and RAG.`,
+  githubLink: "https://github.com/Vimarsh07/MindSutra.git"
+  },
+"E-commerce Website":
+  {
+    description: `
+    Full-stack Development: Developed a responsive and cross-browser compatible E-commerce website using React for the frontend, SQL for efficient data querying, and MongoDB for the database.
+    AWS Deployment: Deployed the application using AWS services, ensuring scalability and reliability.
+    Performance Optimization: Enhanced user experience by reducing latency and improving data processing capabilities through optimized API calls and database queries.`,
+    githubLink: "https://github.com/Vimarsh07/ecommerce-frontend.git"
+  }, 
+  
+  
+  "Data-Driven Sales Forecasting and Inventory Management System":{
     description: `
     Gathered and cleaned sales and inventory data from multiple sources including order logs and customer purchase records, ensuring data integrity for accurate analysis.
     Applied machine learning algorithms like ARIMA and Prophet for time-series forecasting, improving the accuracy of sales predictions by 20%. This helped the company manage inventory levels and reduce the occurrence of stock shortages.
@@ -11,10 +42,6 @@ export const Data = {
     Automated the reporting process to provide weekly insights into sales performance, product demand, and stock levels, reducing manual reporting time by 30%.`
 
    },
-
-
-
-
   "Software Control Metric Analysis (Python)": {
     description: `
     Engineered raw datasets with Python libraries (Numpy, Pandas, SkLearn, Matplotlib), enriched them with new features and synthetic samples
@@ -28,41 +55,9 @@ export const Data = {
     Spearheaded the creation of dynamic dashboards that effectively communicated KPIs, trends, and patterns in Power BI
     Utilizes regression analysis to identify patterns, trends, and factors influencing match results and player performance.
     Implemented Heat-maps to visualize player activity patterns and Scatter plots to uncover correlations between player attributes.
-    Developed time series forecasting models to accurately predict future match outcomes, player performance trends, and team dynamics based on historical data patterns.
-
-    `,
+    Developed time series forecasting models to accurately predict future match outcomes, player performance trends, and team dynamics based on historical data patterns.`,
      },
-  "NLP-Based Sentiment Analysis Platform ":
-    {
-      description: `
-      Develop a comprehensive sentiment analysis platform to process and analyze large datasets of textual content, enabling accurate sentiment classification.
-      Employed Python and libraries such as NLTK, TensorFlow, and Keras to develop the platform. Applied NLP techniques like tokenization, stemming, and lemmatization to preprocess text data and implemented machine learning models for sentiment analysis.  
-      `,
-      githubLink: "https://github.com/Vimarsh07/-Sentiment-Classification-using-IMDB-Data",
-    },
-  "Stock Analysis Transformer": {
-    description: `
-    Developed a stock prediction model using Transformer architecture with TensorFlow, extracting market sentiment from financial news to achieve a 20% improvement in prediction accuracy.
-    Examined historical stock prices, trading volumes, and economic indicators over the past decade; identified key patterns that enhanced the prediction model's accuracy by 20%, improving forecast reliability
-    Implemented a Trading Environment with PPO and RL (Reinforcement learning) based model.
-    Incorporated technical indicators such as Relative Strength Index (RSI) and Moving Average Convergence Divergence (MACD) to refine predictions, significantly enhancing the accuracy of buy, sell, or hold recommendations.
-    Developed and deployed the model using robust machine learning pipelines, ensuring real-time data processing and timely predictions for effective trading strategies.
-    Leveraged generative AI solutions to simulate market scenarios and generate synthetic financial data, enhancing the model's training process and improving its robustness and predictive capabilities.`,
-    githubLink: "https://github.com/Vimarsh07/Stock-Analysis-Transformer",
-  },
-  "MindSutra (Mental Health Chatbot)": {
-    description: `A chatbot for empathetic mental health support using fine-tuned Llama models, prompt engineering and RAG..
-      `,
-    },
-  "E-commerce Website":
-    {
-      description: `
-      Full-stack Development: Developed a responsive and cross-browser compatible E-commerce website using React for the frontend, SQL for efficient data querying, and MongoDB for the database.
-      AWS Deployment: Deployed the application using AWS services, ensuring scalability and reliability.
-      Performance Optimization: Enhanced user experience by reducing latency and improving data processing capabilities through optimized API calls and database queries.     
-      `,
-      
-    },
+ 
     "Driving School Management System":
     {
       description: `
